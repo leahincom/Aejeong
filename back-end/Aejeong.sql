cc09e371356a8883559b9836b5f2b0bd83df7a5c
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-11-24 21:55:39 +0900

    Update Aejeong.sql
    updating and running in the mysql server

--- back-end/Aejeong.sql
+++ back-end/Aejeong.sql
@@ -1,17 +1,10 @@
-----------------"Aejeong.sql" begins------------------
- // Items(제품이름, 동물종류, 제품사진, 성분, 리뷰작성자)
-CREATE TABLE Items
-(ItemName CHAR(50) NOT NULL,
-PetSort CHAR(50) NOT NULL,
-Picture CHAR(255) NOT NULL,
-ComponentsName CHAR(30) NOT NULL,
-Nickname CHAR(50) NOT NULL,
-PRIMARY KEY (ItemName, Picture, ComponentsName, Nickname),
-FOREIGN KEY (ComponentsName) REFERENCES Components(ComponentsName),
-FOREIGN KEY (Nickname) REFERENCES Users(Nickname)
+CREATE TABLE Components
+(ComponentsName CHAR(30) NOT NULL,
+ComponentGrade INT(10) NOT NULL,
+Characteristic TEXT,
+PRIMARY KEY (ComponentsName)
 );
- 
- // Users(아이디, 닉네임, 비밀번호, 생년월일, 성별, 전화번호, 이메일, 강아지, 고양이, 기타1, 기타2, 좋아요한상품, 최근본상품)
+
 CREATE TABLE Users
 (UserID CHAR(30) NOT NULL,
 Nickname CHAR(50) NOT NULL,
@@ -27,7 +20,17 @@ etc2 INT(4),
 PRIMARY KEY (Nickname, UserID)
 );
 
- // Reviews(리뷰작성자, 제품이름, 제품구매날짜, 평점, 장점, 단점, 기타)
+CREATE TABLE Items
+(ItemName CHAR(50) NOT NULL,
+PetSort CHAR(50) NOT NULL,
+Picture CHAR(255) NOT NULL,
+ComponentsName CHAR(30) NOT NULL,
+Nickname CHAR(50) NOT NULL,
+PRIMARY KEY (ItemName, Picture, ComponentsName, Nickname),
+FOREIGN KEY (ComponentsName) REFERENCES Components(ComponentsName),
+FOREIGN KEY (Nickname) REFERENCES Users(Nickname)
+);
+
 CREATE TABLE Reviews
 (Nickname CHAR(50) NOT NULL,
 ItemName CHAR(50) NOT NULL,
@@ -41,7 +44,6 @@ FOREIGN KEY (ItemName) REFERENCES Items(ItemName),
 FOREIGN KEY (Nickname) REFERENCES Users(Nickname)
 );
 
- // Likes
 CREATE TABLE Likes
 (Nickname CHAR(50) NOT NULL,
 ItemName CHAR(50) NOT NULL,
@@ -51,7 +53,6 @@ FOREIGN KEY (Nickname) REFERENCES Users(Nickname),
 FOREIGN KEY (ItemName, Picture) REFERENCES Items(ItemName, Picture)
 );
 
- // Recent
 CREATE TABLE Recent
 (Nickname CHAR(50) NOT NULL,
 ItemName CHAR(50) NOT NULL,
@@ -62,11 +63,7 @@ FOREIGN KEY (Nickname) REFERENCES Users(Nickname),
 FOREIGN KEY (ItemName, Picture) REFERENCES Items(ItemName, Picture)
 );
 
- // Components(성분이름, 성분등급, 특성)
-CREATE TABLE Components
-(ComponentsName CHAR(30) NOT NULL,
-ComponentGrade INT(10) NOT NULL,
-Characteristic TEXT,
-PRIMARY KEY (ComponentsName)
-);
-----------------"Aejeong.sql" ends------------------
+CREATE TABLE item_category
+( category_id INT AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(20) NOT NULL, 
+parent INT DEFAULT NULL ); 
