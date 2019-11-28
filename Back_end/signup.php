abfd9cab8dd564fbcf87f2ab69c9f5bb245db635
KyoungMin25 <55219255+KyoungMin25@users.noreply.github.com>
2019-11-28 23:06:49 +0900

    Modify Missing Parts

--- Back_end/signup.php
+++ Back_end/signup.php
@@ -1,4 +1,45 @@
- FROM Users WHERE UserID='$id'";
+ <!DOCTYPE html>
+<html>
+
+<head>
+<title> Login </title>
+</head>
+
+<body>
+	<?php
+		$id=$_POST['id'];
+		$nick=$_POST['nick'];
+		$password=$_POST['password'];
+		$passwordCheck=$_POST['passwordCheck'];
+		$birth=(int)($_POST['year'].$_POST['month'].$_POST['day']);
+		$gender=$_POST['gender'];
+		$phone=$_POST['phone'];
+		$email=$_POST['email'];
+		$dog=(int)($_POST['dog']);
+		$cat=(int)($_POST['cat']);
+		$etc1Name=$_Post['ext1name'];
+		$etc1=(int)($_POST['etc1']);
+		$etc2Name=$_Post['ext2name'];
+		$etc2=(int)($_POST['etc2']);
+		if($password!=$passwordCheck){
+			echo "비밀번호가 다릅니다.";
+			echo "<a herf=signup.html>back page</a>";
+			exit();
+		}
+		else{
+			$password=md5($_POST['password']);
+		}
+		if($id==NULL || $ncik==NULL || $password==NULL || $birth==NULL || $phone==NULL || $email==NULL){
+			echo "빈칸을 모두 채워주세요.";
+			echo "<a herf=signup.html>back page</a>";
+			exit();
+		}
+		$db=new mysqli('localhost', 'aejeong', 'aejeong123', 'aejeong');
+		if(mysqli_connect_errno()){
+			echo '<p>Error: Could not connect to database. <br/>Please try again later.</p>';
+			exit();
+		}
+		$check_id="SELECT * FROM Users WHERE UserID='$id'";
 		$result_id=$db->query($check_id);
 		if($result_id->num_rows==1){
 			echo "중복된 아이디입니다.";
@@ -33,4 +74,4 @@
 		}
 	?>
 </body>
-</html>
+</html> 
