e352ea380c6d0bd28a4d96a057290b590e44d2fc
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-02 19:30:45 +0900

    Add files via upload
    first draft for web crawling

---
+++ back-end/crawling/dog_shampoo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+
+url = 'http://www.dogpre.com/shop/goods/goods_list.php?category=&search_word=%BC%A4%C7%AA'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
+
+product_list = soup.findAll("span", {'class': {'tit-prdc'}})
+
+num = 1
+for i in product_list:
+    print num, i.text.strip()
+    num = num + 1
