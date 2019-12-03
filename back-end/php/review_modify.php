a0a6303f89e28334f3af06f0792b2370d0315031
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-03 18:01:21 +0900

    Update review_modify.php

--- back-end/php/review_modify.php
+++ back-end/php/review_modify.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
-
+<?php  if (session_status() == PHP_SESSION_NONE) {
+    session_start();}
+    $id=$_SESSION['UserID'];
+    $db=mysqli_connect('localhost', 'aejeong', 'aejeong123', 'aejeong');
+    $result=mysqli_query($db, "SELECT * FROM Reviews WHERE Nickname='$Nickname'");
+    $row=mysqli_fetch_assoc($result);
+?>
 <head>
 <title> Review </title>
 </head>
@@ -11,25 +17,28 @@
 			session_start();
 		}
 
-    $Nickname=$_POST['Nickname'];
-		$Advantage=$_POST['Advantage'];
-		$Weakness=$_POST['Weakness'];
-		$Etc=$_POST['Etc'];
+		$Nickname = $row['Nickname'];
+		$ItemName = $row['ItemName'];
+		$Date = $row['Date'];
+		$Advantage = $_POST['Advantage'];
+		$Weakness  = $_POST['Weakness'];
+		$Etc = $_POST['Etc'];
 
 		$db=mysqli_connect('localhost', 'root', 'skwjdgus', 'aejeong');
 		if(mysqli_connect_errno()){
 			echo "<p>Error: Could not connect to database. <br/>Please try again later.</p>";
 			exit();
 		}
-    if($Advantage==NULL || $Weakness==NULL || $Etc==NULL ){
+    if($Rating == NULL || $Advantage==NULL || $Weakness==NULL || $Etc==NULL ){
 			echo "<script>alert('내용을 입력하세요.');history.back(-1);</script>";
 			exit;
 		}
     else {
-				mysqli_query($db,"UPDATE Users SET Nickname='$nick' WHERE UserID='$id'");
-				mysqli_query($db,"UPDATE Users SET Password='$password' WHERE UserID='$id'");
-				mysqli_query($db,"UPDATE Users SET Birth='$birth' WHERE UserID='$id'");
-		      echo "<script>alert('리뷰가 작성되었습니다.'); location.replace('myReview.html'); </script>";
+				mysqli_query($db,"UPDATE Reviews SET Rating='$Rating' WHERE Nickname='$Nickname'");
+				mysqli_query($db,"UPDATE Reviews SET Advantage='$Advantage' WHERE Nickname='$Nickname'");
+				mysqli_query($db,"UPDATE Reviews SET Weakness='$Weakness' WHERE Nickname='$Nickname'");
+				mysqli_query($db,"UPDATE Reviews SET Etc='$Etc' WHERE Nickname='$Nickname'");
+		      echo "<script>alert('리뷰가 수정되었습니다.'); location.replace('myReview.php'); </script>";
   			exit;
   	}
 	?>
