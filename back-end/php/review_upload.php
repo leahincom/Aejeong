f23d76a0bb262beb5f1dbb5a9dd3ee86b7b2c1d6
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-03 18:00:51 +0900

    Update review_upload.php

--- back-end/php/review_upload.php
+++ back-end/php/review_upload.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
 <html>
-
+<?php  if (session_status() == PHP_SESSION_NONE) {
+    session_start();}
+    $id=$_SESSION['UserID'];
+    $db=mysqli_connect('localhost', 'aejeong', 'aejeong123', 'aejeong');
+    $result=mysqli_query($db, "SELECT * FROM Reviews WHERE Nickname='$Nickname'");
+    $row=mysqli_fetch_assoc($result);
+?>
 <head>
 <title> review </title>
 </head>
 
 <body>
 	<?php
-		$Nickname=$_POST['Nickname'];
-		$Advantage=$_POST['Advantage'];
-		$Weakness=$_POST['Weakness'];
-		$Etc=$_POST['Etc'];
-
-		$db=mysqli_connect('localhost', 'root', 'skwjdgus', 'aejeong');
+$Nickname = $row['Nickname'];
+$ItemName = $row['ItemName'];
+$Date = $row['Date'];
+$Advantage = $_POST['Advantage'];
+$Weakness  = $_POST['Weakness'];
+$Etc = $_POST['Etc'];
+		$db=new mysqli('localhost', 'aejeong', 'aejeong123', 'aejeong');
 		if(mysqli_connect_errno()){
-			echo '<p>Error: Could not connect to database. <br/>Please try again later.</p>';
+			echo '<p>Error: Could not connect to database.<br/>
+				Please try again later.</p>';
 			exit;
 		}
-
-		if($Advantage==NULL || $Weakness==NULL || $Etc==NULL ){
-			echo "<script>alert('내용을 입력하세요.');history.back(-1);</script>";
+		if($Rating == NULL || $Advantage==NULL || $Weakness==NULL || $Etc==NULL ){
+			echo "<script>alert('내용을 입력하세요.');
+			history.back(-1); </script>";
 			exit;
 		}
-
-		$sql="INSERT INTO Reviews(Nickname, Advantage, Weakness, Etc)
-		VALUES ('$Nickname', '$Advantage', '$Weakness', '$Etc')";
-
+		$sql="INSERT INTO reviews(Nickname, ItemName, Date, Rating, Advantage, Weakness, Etc)
+		VALUES ('$Nickname', '$ItemName', '$Date', '$Rating', '$Advantage', '$Weakness', '$Etc')";
 		if(mysqli_query($db, $sql)){
-			echo "<script>alert('리뷰가 작성되었습니다.'); location.replace('myReview.html'); </script>";
+			echo "<script>alert('리뷰가 작성되었습니다.');
+			location.replace('myReview.php'); </script>";
 			exit;
 		}
 	?>
