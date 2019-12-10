0a0ae1cfb8384ea6c48cbb7fd78894168f1c250f
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-10 18:05:39 +0900

    Add files via upload

--- back-end/crawling/cat_shampoo_review.py
+++ back-end/crawling/cat_shampoo_review.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=22157'
+url = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=9004'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
@@ -19,20 +19,21 @@ while 1:
 
 
 idx = 0
-for page_number in range(1, maximum+1):
+for page_number in range(1, maximum + 1):
     pURL = url + '&amp;page=' + str(page_number)
-    presponse = requests.get(pURL)
-    psoup = BeautifulSoup(presponse.content, 'html.parser', from_encoding='utf-8')
-    writer_list = psoup.findAll("span", {'class': {'name'}});
-    esti_list = psoup.findAll("span",{'class': {'ic-star'}});
-    review_list = psoup.findAll("p",{'class': {'comment comment-bg'}});
+    response = requests.get(pURL)
+    soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
+    writer_list = soup.findAll("span", {'class': {'name'}});
+    esti_list = soup.findAll("span", {'class': {'ic-star'}});
+    review_list = soup.findAll("p", {'class': {'comment comment-bg'}});
     for esti in esti_list:
         if(esti.get_text().strip() >= 4):
-            print "장점|", writer_list[idx].get_text().strip(), "|", esti_list[idx].get_text().strip(), "|", review_list[idx].get_text().strip()
-            idx = idx+1
+            print "장점|", writer_list[idx].get_text().strip(), "|", esti.get_text(
+            ).strip(), "|", review_list[idx].get_text().strip()
+            idx = idx + 1
          elif(esti.get_text().strip() < 3):
-            print "단점|", writer_list[idx].get_text().strip(), "|", esti_list[idx].get_text().strip(), "|", review_list[idx].get_text().strip()
+            print "단점|", writer_list[idx].get_text().strip(), "|", esti.get_text().strip(), "|", review_list[idx].get_text().strip()
             idx = idx+1
          else:
-            print "기타|", writer_list[idx].get_text().strip(), "|", esti_list[idx].get_text().strip(), "|", review_list[idx].get_text().strip()
+            print "기타|", writer_list[idx].get_text().strip(), "|", esti.get_text().strip(), "|", review_list[idx].get_text().strip()
             idx = idx+1
