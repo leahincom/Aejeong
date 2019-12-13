620cff2d3919fce033957a44610f487dd1275222
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-13 14:54:13 +0900

    Add files via upload

--- back-end/crawling/cat_shampoo_review.py
+++ back-end/crawling/cat_shampoo_review.py
@@ -1,39 +1,30 @@
-# -*- coding: utf-8 -*-
-
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-url = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=9004'
-response = requests.get(url)
-
+URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=24978'
+response = requests.get(URL)
 soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
 
-maximum = 0
-page = 1
-while 1:
-	page_list = soup.findAll("a", {"class": "number"})
-	if not page_list:
-		maximum = page - 1
-		break
-	page = page + 1
+page_list = soup.findAll("a", {"class": {'number'}})
 
-
-idx = 0
-for page_number in range(1, maximum + 1):
-    pURL = url + '&amp;page=' + str(page_number)
-    response = requests.get(pURL)
+for page_number in range(0, len(page_list)+1):
+    URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=24978&page=' + str(page_number)
+    response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
-    writer_list = soup.findAll("span", {'class': {'name'}});
-    esti_list = soup.findAll("span", {'class': {'ic-star'}});
-    review_list = soup.findAll("p", {'class': {'comment comment-bg'}});
+    writer_list = soup.findAll("span", {'class': {'name'}})
+    esti_list = soup.findAll("span", {'class': {'ic-star'}})
+    review_list = soup.findAll("p", {'class': {'comment comment-bg'}})
+    idx = 0
     for esti in esti_list:
-        if(esti.get_text().strip() >= 4):
-            print "장점|", writer_list[idx].get_text().strip(), "|", esti.get_text(
-            ).strip(), "|", review_list[idx].get_text().strip()
-            idx = idx + 1
-         elif(esti.get_text().strip() < 3):
-            print "단점|", writer_list[idx].get_text().strip(), "|", esti.get_text().strip(), "|", review_list[idx].get_text().strip()
+        score_string = esti.get_text().strip()
+        score_string = score_string[:-1]
+        score = float(score_string)
+        if(score >= 4):
+            print "장점|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
             idx = idx+1
-         else:
-            print "기타|", writer_list[idx].get_text().strip(), "|", esti.get_text().strip(), "|", review_list[idx].get_text().strip()
+        elif(score < 3):
+            print "단점|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
+            idx = idx+1
+        else:
+            print "기타|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
             idx = idx+1
