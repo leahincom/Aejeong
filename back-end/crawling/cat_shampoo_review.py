3b6bc35bedf8a4c65f66d30746128f5970a9c371
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-13 16:00:43 +0900

    Add files via upload

--- back-end/crawling/cat_shampoo_review.py
+++ back-end/crawling/cat_shampoo_review.py
@@ -1,30 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=24978'
+URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=23213'
 response = requests.get(URL)
 soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
 
 page_list = soup.findAll("a", {"class": {'number'}})
 
-for page_number in range(1, len(page_list)+1):
-    URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=24978&page=' + str(page_number)
+for page_number in range(0, len(page_list)+1):
+    URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=23213&page=' + str(page_number+1)
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
     writer_list = soup.findAll("span", {'class': {'name'}})
+    date_list = soup.findAll("span", {"class": {"date"}})
     esti_list = soup.findAll("span", {'class': {'ic-star'}})
     review_list = soup.findAll("p", {'class': {'comment comment-bg'}})
     idx = 0
     for esti in esti_list:
+        writer = writer_list[idx].get_text().strip()
+        writer = writer[4:]
         score_string = esti.get_text().strip()
         score_string = score_string[:-1]
         score = float(score_string)
         if(score >= 4):
-            print "장점|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
+            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
             idx = idx+1
         elif(score < 3):
-            print "단점|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
+            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", review_list[idx].get_text().strip()
             idx = idx+1
         else:
-            print "기타|", writer_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
+            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", "|", review_list[idx].get_text().strip()
             idx = idx+1
