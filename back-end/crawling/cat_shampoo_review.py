a02b9b688e33932cd3ad9a1a14aca33290b6a65b
yesyoumay <49134038+yesyoumay@users.noreply.github.com>
2019-12-13 16:34:00 +0900

    Add files via upload

--- back-end/crawling/cat_shampoo_review.py
+++ back-end/crawling/cat_shampoo_review.py
@@ -7,27 +7,26 @@ soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
 
 page_list = soup.findAll("a", {"class": {'number'}})
 
+iden = 0
 for page_number in range(0, len(page_list)+1):
     URL = 'http://www.catpre.com/shop/goods/goods_post.php?goodsno=23213&page=' + str(page_number+1)
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
-    writer_list = soup.findAll("span", {'class': {'name'}})
     date_list = soup.findAll("span", {"class": {"date"}})
     esti_list = soup.findAll("span", {'class': {'ic-star'}})
     review_list = soup.findAll("p", {'class': {'comment comment-bg'}})
     idx = 0
     for esti in esti_list:
-        writer = writer_list[idx].get_text().strip()
-        writer = writer[4:]
+        iden = iden+1
         score_string = esti.get_text().strip()
         score_string = score_string[:-1]
         score = float(score_string)
         if(score >= 4):
-            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
+            print iden, "|하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", review_list[idx].get_text().strip()
             idx = idx+1
         elif(score < 3):
-            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", review_list[idx].get_text().strip()
+            print iden, "|하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", review_list[idx].get_text().strip()
             idx = idx+1
         else:
-            print writer, "| 하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", "|", review_list[idx].get_text().strip()
+            print iden, "|하이포닉 워터리스 샴푸 고양이용 190ml |", date_list[idx].get_text().strip(), "|", score, "|", "|", "|", review_list[idx].get_text().strip()
             idx = idx+1
